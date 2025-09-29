4d0144788be3d753e592515b72c8d86b9d5ce326
Laxit Savaliya <laxitsavaliya23@gmail.com>
2025-09-29 21:27:35 +0530

    Update frontend components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,22 @@
+import { lazy, Suspense } from 'react';
 import { Navigate, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import SignUpPage from './Pages/SignUpPage';
-import LoginPage from './Pages/LoginPage';
-import HomePage from './Pages/HomePage';
-import OnboardingPage from './Pages/OnboardingPage';
-import ProfilePage from './Pages/ProfilePage';
-import Notification from './Pages/Notification';
-import PostPage from './Pages/PostPage';
-import UpdateProfile from './Pages/UpdateProfile';
-import PageNotFound from './Pages/PageNotFound';
-
 import PageLoader from './Components/PageLoader';
 import Layout from './Components/Layout';
 import useAuthUser from './Hooks/useAuthUser';
-import ChatPage from './Pages/ChatPage';
+
+const SignUpPage = lazy(() => import('./Pages/SignUpPage'));
+const LoginPage = lazy(() => import('./Pages/LoginPage'));
+const HomePage = lazy(() => import('./Pages/HomePage'));
+const OnboardingPage = lazy(() => import('./Pages/OnboardingPage'));
+const ProfilePage = lazy(() => import('./Pages/ProfilePage'));
+const Notification = lazy(() => import('./Pages/Notification'));
+const PostPage = lazy(() => import('./Pages/PostPage'));
+const UpdateProfile = lazy(() => import('./Pages/UpdateProfile'));
+const PageNotFound = lazy(() => import('./Pages/PageNotFound'));
+const ChatPage = lazy(() => import('./Pages/ChatPage'));
 
 // --- Protected Route Wrapper ---
 const ProtectedRoute = ({ children }) => {
@@ -44,26 +45,28 @@ const App = () => {
 
   return (
     <div className='h-screen gradient-bg'>
-      <AnimatePresence exitBeforeEnter>
-        <Routes>
-          {/* Public Routes */}
-          <Route path='/signup' element={<PageMotion><SignUpPageRedirect /></PageMotion>} />
-          <Route path='/login' element={<PageMotion><LoginPageRedirect /></PageMotion>} />
-          <Route path='/onboarding' element={<PageMotion><OnboardingRedirect /></PageMotion>} />
+      <Suspense fallback={<PageLoader />}>
+        <AnimatePresence exitBeforeEnter>
+          <Routes>
+            {/* Public Routes */}
+            <Route path='/signup' element={<PageMotion><SignUpPageRedirect /></PageMotion>} />
+            <Route path='/login' element={<PageMotion><LoginPageRedirect /></PageMotion>} />
+            <Route path='/onboarding' element={<PageMotion><OnboardingRedirect /></PageMotion>} />
 
-          {/* Protected Routes */}
-          <Route path='/' element={<ProtectedRoute><Layout showRightSidebar><PageMotion><HomePage /></PageMotion></Layout></ProtectedRoute>} />
-          <Route path='/profile/:userName' element={<ProtectedRoute><Layout><PageMotion><ProfilePage /></PageMotion></Layout></ProtectedRoute>} />
-          <Route path='/notifications' element={<ProtectedRoute><Layout><PageMotion><Notification /></PageMotion></Layout></ProtectedRoute>} />
-          <Route path='/post/:postId' element={<ProtectedRoute><Layout><PageMotion><PostPage /></PageMotion></Layout></ProtectedRoute>} />
-          <Route path='/profile/:userName/update-profile' element={<ProtectedRoute><Layout><PageMotion><UpdateProfile /></PageMotion></Layout></ProtectedRoute>} />
-          <Route path='/chat' element={<ProtectedRoute><Layout><PageMotion><ChatPage /></PageMotion></Layout></ProtectedRoute>} />
-          <Route path='/chat/:userId' element={<ProtectedRoute><Layout><PageMotion><ChatPage showChat /></PageMotion></Layout></ProtectedRoute>} />
+            {/* Protected Routes */}
+            <Route path='/' element={<ProtectedRoute><Layout showRightSidebar><PageMotion><HomePage /></PageMotion></Layout></ProtectedRoute>} />
+            <Route path='/profile/:userName' element={<ProtectedRoute><Layout><PageMotion><ProfilePage /></PageMotion></Layout></ProtectedRoute>} />
+            <Route path='/notifications' element={<ProtectedRoute><Layout><PageMotion><Notification /></PageMotion></Layout></ProtectedRoute>} />
+            <Route path='/post/:postId' element={<ProtectedRoute><Layout><PageMotion><PostPage /></PageMotion></Layout></ProtectedRoute>} />
+            <Route path='/profile/:userName/update-profile' element={<ProtectedRoute><Layout><PageMotion><UpdateProfile /></PageMotion></Layout></ProtectedRoute>} />
+            <Route path='/chat' element={<ProtectedRoute><Layout><PageMotion><ChatPage /></PageMotion></Layout></ProtectedRoute>} />
+            <Route path='/chat/:userId' element={<ProtectedRoute><Layout><PageMotion><ChatPage showChat /></PageMotion></Layout></ProtectedRoute>} />
 
-          {/* 404 */}
-          <Route path='*' element={<PageMotion><PageNotFound /></PageMotion>} />
-        </Routes>
-      </AnimatePresence>
+            {/* 404 */}
+            <Route path='*' element={<PageMotion><PageNotFound /></PageMotion>} />
+          </Routes>
+        </AnimatePresence>
+      </Suspense>
       <Toaster />
     </div>
   );
@@ -77,6 +80,7 @@ const PageMotion = ({ children }) => (
     exit='out'
     variants={pageVariants}
     transition={pageTransition}
+    style={{ height: '100%' }}
   >
     {children}
   </motion.div>
